1c278b8092764892c6c9f4fef0428bdc8ba30d44
Oleksandr Palamarchuk <sashap740@gmail.com>
2022-05-06 08:47:58 +0200

    created ItemPostEntity

---
+++ src/main/java/ua/palamar/courseworkbackend/entity/post/ItemPostEntity.java
@@ -0,0 +1,19 @@
+package ua.palamar.courseworkbackend.entity.post;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemPostEntity extends Post{
+
+    
+
+}
