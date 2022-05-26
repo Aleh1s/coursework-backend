379e2d2a56a1e7c3891220134b22fb61a684f1fb
Oleksandr Palamarchuk <sashap740@gmail.com>
2022-05-26 14:18:39 +0300

    changed entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,1 @@
-/usr/bin/java -XX:+UseContainerSupport -Xmx256m -Xss512k -XX:MetaspaceSize=100m -jar /apps/app.jar
+/usr/bin/java -XX:+UseContainerSupport -Xmx256m -Xss512k -XX:MetaspaceSize=100m -Djasypt.encryptor.password=palamar -jar /apps/app.jar
